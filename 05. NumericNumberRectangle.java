156423ea86e5664da39de1407593aecc5a38c17e
vaibhavsushir <sushirvaibhav523@gmail.com>
2026-02-03 19:23:42 +0530

    Add files via upload

---
+++ 05. NumericNumberRectangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ class NumericNumberRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r = input.nextInt();
+        int c = input.nextInt();
+
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j <= c ; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(2);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
