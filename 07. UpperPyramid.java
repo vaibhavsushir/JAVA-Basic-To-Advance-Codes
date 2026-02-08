2601e85a599e7713dd1fb912eb4543a1367afcd7
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-08 20:30:20 +0530

    Add title output for upper pyramid display
    Added a title print statement before the pyramid output.

--- 07. UpperPyramid.java
+++ 07. UpperPyramid.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int r = input.nextInt();
+     System.out.println("Upper pyramid");
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= (r + 1 - i); j++) {
                     System.out.print("*");
@@ -12,4 +13,5 @@ import java.util.Scanner;
 
             }
     }
+
 
