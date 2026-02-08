8757d151b0d4cb595fcb5845ee09af415b0cb2cb
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-08 20:31:10 +0530

    Enhance output with 'Right Pyramid' title
    Added a title print statement for clarity.

--- 06. Rightpyramid.java
+++ 06. Rightpyramid.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int r = input.nextInt();
+     System.out.println("Right Pyramid");
     for (int i = 1; i <= r; i++) {
         for (int j = 1; j <= i; j++) {
             System.out.print("*");
@@ -12,3 +13,4 @@ import java.util.Scanner;
 
         }
 }
+
