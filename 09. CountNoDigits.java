1286e39631fa526740b5e62d84d6947975a869fd
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-09 12:40:46 +0530

    Add files via upload

---
+++ 09. CountNoDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ class CountNoDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        int count = noofdigits(num);
+        System.out.println("Your Number Of Digits Is: "+count);
+    }
+    public static int noofdigits(int num){
+        int NOD = 0;
+        while(num > 0){
+            NOD += 1;
+            num /= 10;
+        }
+        return  NOD;
+    }
+}
