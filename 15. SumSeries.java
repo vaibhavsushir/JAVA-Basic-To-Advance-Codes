152794fc8899fe2fbef2fbb6cfcfa2ff7ae85b06
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-17 07:55:01 +0530

    Add files via upload

---
+++ 15. SumSeries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ class SumSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number Till You Want Sum : ");
+        int num = input.nextInt();
+
+        int sum = 0;
+        for( int i = 1 ; i <= num ; i++){
+            if( i % 2 == 0){
+                sum = sum - i ;
+            }else{
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
