967cdae10a5b1c9d2615f717df5b5fd03834f9b7
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-21 08:34:07 +0530

    Add files via upload

---
+++ 17. FindTotalTargetSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class FindTotalTargetSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numarr = {1,2,3,4,5,6,7,8};
+        int target = 7;
+        int totaltarget = totaltargetsum(numarr,target);
+        System.out.println("Total Number Of Target Found Is: "+totaltarget);
+    }
+    public static int totaltargetsum(int[] numarr,int target){
+        int total = 0;
+        for(int i=0; i < numarr.length; i++){
+            for(int j=i+1; j < numarr.length; j++){
+                if(numarr[i] + numarr[j] == target ){
+                    total = total + 1;
+                }
+            }
+        }
+        return total;
+    }
+}
