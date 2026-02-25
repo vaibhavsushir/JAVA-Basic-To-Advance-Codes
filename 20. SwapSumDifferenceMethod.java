09dcfbec0caceba7b889093e268f07d8e16c101c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-25 07:42:42 +0530

    Update 20. SwapSumDifferenceMethod.java

--- 20. SwapSumDifferenceMethod.java
+++ 20. SwapSumDifferenceMethod.java
@@ -1,6 +1,6 @@
 class SwapSumDifferenceMethod {
     public static void main(String[] args) {
-        System.out.println("Swap dSum Difference Method");
+        System.out.println("Swap Sum Difference Method");
         System.out.println("Before Swap");
         int a = 9;
         int b = 3;
@@ -15,4 +15,5 @@ class SwapSumDifferenceMethod {
         System.out.println("After Swap: a = "+a+" And b = "+b);
     }
 }
+
 
