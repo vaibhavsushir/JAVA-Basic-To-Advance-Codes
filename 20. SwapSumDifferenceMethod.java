e18f9876b0a0fe741b8f641c0ac07a13acb5cf84
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-25 07:41:32 +0530

    Add files via upload

---
+++ 20. SwapSumDifferenceMethod.java
@@ -0,0 +1,16 @@
+class SwapSumDifferenceMethod {
+    public static void main(String[] args) {
+        System.out.println("Before Swap");
+        int a = 9;
+        int b = 3;
+        System.out.println("Before Swap: a = "+a+" And b = "+b);
+        swap(a,b);
+
+    }
+    static void swap(int a, int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swap: a = "+a+" And b = "+b);
+    }
+}
