23c6abae1b5d6f0ba478445d33572b48a9d2c8df
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-21 22:34:07 +0530

    Update 19. SwapTwoNumbers.java

--- 19. SwapTwoNumbers.java
+++ 19. SwapTwoNumbers.java
@@ -1,5 +1,5 @@
 class SwapTwoNumbers {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Swap Two Numbers");
         int a = 3;
         int b = 9;
