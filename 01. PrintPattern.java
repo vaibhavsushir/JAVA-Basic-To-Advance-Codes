06cf4c29c712d58ced8a244c330f37ff03151842
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-07 23:10:59 +0530

    Update 01. PrintPattern.java

--- 01. PrintPattern.java
+++ 01. PrintPattern.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class PrintPattern {
+class PrintPattern{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int r = input.nextInt();
