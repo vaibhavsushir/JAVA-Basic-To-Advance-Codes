5f881dd44cb6ae84cf80e83805ae759e666de143
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-07 23:11:47 +0530

    Update 11. occorences.java

--- 11. occorences.java
+++ 11. occorences.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class occorences {
+ class occorences{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
      System.out.println("Occurence A Number In Array");
